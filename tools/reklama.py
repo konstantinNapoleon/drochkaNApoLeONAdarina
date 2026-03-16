b1b697e39193842047397dae6c752a33bd84927c
konstantinNapoleon <konstantinkukuskin165@gmail.com>
2026-03-17 02:18:22 +0300

    воаоаоа

--- tools/reklama.py
+++ tools/reklama.py
@@ -17,7 +17,7 @@ async def cmd_post(message: types.Message, bot: Bot, get_all_users):
         return await message.answer("❌ У тебя нет прав администратора.")
 
     # --- НАСТРОЙКИ РАССЫЛКИ ---
-    PHOTO_URL = "https://yapx.ru/94e43ee6-5474-4645-9121-62670c6a6505.jpg"
+    PHOTO_URL = "https://ibb.co/B2FsgtLq.jpg"
     link_text = "Если вас нет в наших ресурсах, то скорее нажминайте кнопку и вступайте 👇"
 
     # Создаем кнопки напрямую через InlineKeyboardMarkup (так надежнее)
